84f7e2261186b3b73c225b3353f9bfac5911d13e
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-10-10 16:29:41 +0200

    function structure fixed

--- www/js/userajax.js
+++ www/js/userajax.js
@@ -1,35 +1,17 @@
-
-const url = "http://localhost:8000/";
-const user = "testadmin";
-const pass = "test123";
-
-function connectJson() {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            console.log("Status: " + xhr.status);
-            console.log(xhr.response);
-
-
-            
+function getUserInfo() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() 
+    {
+        if(this.readyState == 4 && this.status == 200) 
+        {
+            var user = JSON.parse(xhr.response);
+            console.log(user);
+            $("#userName").html(user['name']);
         }
     }
 
-    return xhr;
-}
-
-function getUserInfo() {
-    let xhr = connectJson();
-
-    xhr.open("GET", url+"json/get/user", true, user, pass);
+    xhr.open("GET", "http://localhost:8000/json/get/user", true, "testadmin", "test123");
     xhr.send();
-
-    let user = JSON.parse(xhr.response);
-
-    console.log(typeof(user));
-    console.log(user["name"]);
-
-    $("#userName").html(user["name"]);
 }
 
 getUserInfo();
