70b3b78f9c038f90df78d91b9d4348f8ec3307d1
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-10-22 11:32:55 +0200

    blanked credentials, changed config from constants to normal variables

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
-const SERVER_URL = "http://localhost:8000"; 
+SERVER_URL = "http://localhost:8000"; 
 
 // Local credentials
-const LOCAL_USER = "testadmin";
-const LOCAL_PASS = "test123";
-const LOCAL_CRED = {LOCAL_USER, LOCAL_PASS};
+LOCAL_USER = "";
+LOCAL_PASS = "";
+LOCAL_CRED = {LOCAL_USER, LOCAL_PASS};
