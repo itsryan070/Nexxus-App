fe22fc3f651c21ca60aa363ae33ca82e8d44896c
Ben Wolthuis <ben.wolthuis@copiatek.nl>
2018-11-20 14:27:21 +0100

    mvc time

--- www/js/userauth.js
+++ www/js/userauth.js
@@ -1,40 +1,49 @@
-$(document).ready(function(){
-    var logged_in = false;
+if(LOCAL_USER!='') {
+    orders_index();
+}
+else {
+    window.location.url = "loginprocess.html";
+}
 
-    // get user data
-    $.post(SERVER_URL + "/json/auth/user",
-    {
-        name: LOCAL_USER,
-        pass: LOCAL_PASS,
-    },
-    function(data, status) {
-        console.log("test");
+function orders_index()
+{
+    $(document).ready(function(){
+        var logged_in = false;
 
-        if(status=='success') {
-            result = data;
-            logged_in = true;
-        } else {
-            console.log(status);
-            //window.location.href = "loginprocess.html";
-        }
-
-        $("#userName").html(result.username);
-        $("#userEmail").html(result.email);
-
-        if(logged_in) {
-            // get orders
-            $.post(SERVER_URL + "/json/get/orders",
+        if(LOCAL_USER!='') 
+        {
+            // get user data
+            $.post(SERVER_URL + "/json/auth/user",
             {
                 name: LOCAL_USER,
                 pass: LOCAL_PASS,
             },
             function(data, status) {
-                var table = "";
-                $.each(data, function(index, value) {
-                    console.log(index +": "+value);
-                });
+                console.log("test");
+
+                if(status=='success') {
+                    result = data;
+                    logged_in = true;
+                } 
+                $("#userName").html(result.username);
+                $("#userEmail").html(result.email);
+
+                if(logged_in) {
+                    // get orders
+                    $.post(SERVER_URL + "/json/get/orders",
+                    {
+                        name: LOCAL_USER,
+                        pass: LOCAL_PASS,
+                    },
+                    function(data, status) {
+                        var table = "";
+                        $.each(data, function(index, value) {
+                            console.log(index +": "+value);
+                        });
+                    });
+                }
             });
-        }
+        } 
     });
-});
+}
 
