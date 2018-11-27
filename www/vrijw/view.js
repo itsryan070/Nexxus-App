2560f24ee82d1ded8e0baffa1fc0e22980871512
nebbii <ben.wolthuis@copiatek.nl>
2018-11-27 14:36:54 +0100

    obtains data, html formatting is not correct yet

--- www/vrijw/view.js
+++ www/vrijw/view.js
@@ -7,36 +7,38 @@ class View
 
     showHeader(div)
     {
-        $(div).append("<div data-role='header' data-position='fixed'>"
-			+ "<h1 >Nexxus</h1>"
-		+ "</div>"
-		+ "<div class='ui-resize' data-role='content' data-theme='a'>"
-			+ "<h3 style='margin:0;margin-left:2vw; margin-top:1vh;'> Aangeboden taken </h3>"
-		
+        $(div).append("<div data-role='header' data-position='fixed' role='banner' class='ui-header ui-bar-inherit ui-header-fixed slidedown'>"
+              + "<h1 class='ui-title' role='heading' aria-level='1'>Nexxus</h1>");
     }
 
     showOfferedTasks(div, tasks)
     {
-        $(div).append("<div class='ui-resize ui-content ui-body-a' data-role='content' data-theme='a' role='main'>"
-            + "<table id='table-offered-tasks' data-role='table' class='ui-responsive ui-table ui-table-reflow'>");
+        var html = "";
+        /* body */
+        html  += "<div class='ui-resize' data-role='content' data-theme='a'>"
+                + "<h3 style='margin:0;margin-left:2vw; margin-top:1vh;'> Aangeboden taken </h3>";
 
-		$("#table-offered-tasks").append(""
+        /* table */
+        html += "<table id='table-offered-tasks' data-role='table' class='ui-responsive ui-table ui-table-reflow'>"
             + "<tbody id='title'><tr>"
             +   "<td><b>Datum</b></td>"
             +   "<td><b>Hoeveelheid</b></td>"
             +   "<td><b>Stad</b></td>"
-            + "</tr>"
-        );
-        // return rows
-		for(var i=0; i < tasks.length; i++){
-			$("#table-offered-tasks").append("<tr onclick=" 
-                + "controller.Setinfoselected('"+i+"') data-priority='1' id='title"+i+"'>"
-                +   "<td>Test</td>" 
-                + "</tr>"
-            );
+            + "</tr>";
+
+        /* table rows */
+		for(var i=0; i < tasks.length; i++) 
+        {
+			html += "<tr onclick='' data-priority='1' id='title"+i+"'>"
+                +   "<td>" + tasks[i]['date'] + "</td>" 
+                +   "<td>" + tasks[i]['quantity'] + "</td>" 
+                +   "<td>" + tasks[i]['location']['name'] + "</td>" 
+                + "</tr>";
             console.log(tasks[i]);
 		}	
-		$("#table-offered-tasks").append("</tbody>");
+        html += "</tbody>";
+
+        $(div).append(html);
     }
 
     showAcceptedTasks()
