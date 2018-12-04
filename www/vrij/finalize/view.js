ea0d0d92fce9be8721d5c0f58baa403b9edb7edc
nebbii <ben.wolthuis@copiatek.nl>
2018-12-04 16:46:26 +0100

    images get added by the given quantity

--- www/vrij/finalize/view.js
+++ www/vrij/finalize/view.js
@@ -14,6 +14,25 @@ class FinalizeView
     showFotoForm()
     {
         $("#quantity-form") .hide();
-        $("#foto-form")     .fadeIn();
+        // add images according to amount
+        var imageOption = ""; 
+        imageOption += "<div class='image-upload'>";
+            imageOption += "<label for='file-input'>";
+            imageOption += "<img src='include/img/plus.png' class='ui-plus'/>";
+            imageOption += "</label>";
+
+            imageOption += "<input id='file-input' type='file'/>";
+        imageOption += "</div>";
+
+        var quantity = $("#itemQuantity").val();
+        for(var i=0;i<quantity;i++) 
+        {
+            $("#foto-form").append(imageOption);
+        }
+        var html = "<br><br><input type='submit' name='submit'>";
+        $("#foto-form").append(html);
+        $("#foto-form").fadeIn();
     }
+
+
 }
