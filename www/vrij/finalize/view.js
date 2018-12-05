bf9f8c5f5f7c3a383e004a0a55135a3f975afeee
nebbii <ben.wolthuis@copiatek.nl>
2018-12-05 15:21:38 +0100

    added modulo func for organizing icons

--- www/vrij/finalize/view.js
+++ www/vrij/finalize/view.js
@@ -14,23 +14,38 @@ class FinalizeView
     showFotoForm()
     {
         $("#quantity-form") .hide();
+
         // add images according to amount
         var imageOption = ""; 
-        imageOption += "<div class='image-upload'>";
+        imageOption += "<td><div class='image-upload'>";
             imageOption += "<label for='file-input'>";
             imageOption += "<img src='include/img/plus.png' class='ui-plus'/>";
             imageOption += "</label>";
 
             imageOption += "<input id='file-input' class='foto-input' type='file'/>";
-        imageOption += "</div>";
+        imageOption += "</div></td>";
 
         var quantity = $("#itemQuantity").val();
 
         if(quantity>999) { quantity = 999; }
+
+        var htmlfoto = "";
+
         for(var i=0;i<quantity;i++) 
         {
-            $("#foto-icons").append(imageOption);
+            var input = "";
+            switch(i % 2) 
+            {
+                case 0:
+                    input = "<tr>" + imageOption; 
+                    break;
+                case 1:
+                    input = imageOption + "</tr>"; 
+                    break;
+            }
+            htmlfoto += input;
         }
+        $("#foto-icons").html(htmlfoto);
 
         $("#foto-form").fadeIn();
     }
