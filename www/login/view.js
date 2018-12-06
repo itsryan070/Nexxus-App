9b1c4e9a76f8e30d6f57f061fd7b17e1e23cd7cb
nebbii <ben.wolthuis@copiatek.nl>
2018-12-06 14:30:28 +0100

    cleaned up login html

--- www/login/view.js
+++ www/login/view.js
@@ -7,29 +7,32 @@ class LoginView
 
     showLoginForm(div)
     {
-        $("body").html("<div id='page1' data-role='page' data-theme='a'>"
-                        +'<div data-role="header" data-position="fixed" role="banner" class="ui-header ui-bar-inherit slidedown">'
-                            +"<h1 class='ui-title'>Nexxus</h1>"
-                        +"</div>"
+        var content = "";
+        content += "<div id='page1' data-role='page' data-theme='a'>"
+                    +'<div data-role="header" data-position="fixed" role="banner" class="ui-header ui-bar-inherit slidedown">'
+                        +"<h1 class='ui-title'>Nexxus</h1>"
+                    +"</div>"
               
   
-        + '<div data-role="content" data-theme="a" class="ui-content ui-body-a" role="main">'
-            + "<h2>Welcome to Nexxus</h2>"
-            + "<form id='loginform' name='loginform'>"
-            + "<label for='username'>Username</label>"
-                + "<div class='form-group ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset'>"       
-                    + "<input type='text' id='username' class='form-control' placeholder='Username'>"
+                + '<div data-role="content" data-theme="a" class="ui-content ui-body-a" role="main">'
+                    + "<h2>Welcome to Nexxus</h2>"
+                    + "<form id='loginform' name='loginform'>"
+                    + "<label for='username'>Username</label>"
+                        + "<div class='form-group ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset'>"       
+                            + "<input type='text' id='username' class='form-control' placeholder='Username'>"
+                        + "</div>"
+                         + "<label for='password'>Password</label>"
+                        + "<div class='form-group ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset'>"
+                            + "<input type='password' id='password' name='password' class='form-control' placeholder='********'>"
+                        + "</div>"
+                        + "<div class='form-group'>"
+                            + "<button type='submit' id='btn-submit' class='ui-btn-login ui-btn'>Login</button>"
+                        + "</div>"
+                    + "</form>"
                 + "</div>"
-                 + "<label for='password'>Password</label>"
-                + "<div class='form-group ui-input-text ui-body-inherit ui-corner-all ui-shadow-inset'>"
-                    + "<input type='password' id='password' name='password' class='form-control' placeholder='********'>"
-                + "</div>"
-                + "<div class='form-group'>"
-                    + "<button type='submit' id='btn-submit' class='ui-btn-login ui-btn'>Login</button>"
-                + "</div>"
-            + "</form>"
-        + "</div>");
-        +"</div>"
+            +"</div>");
+
+        $("body").html(content);
     }
 
 }
