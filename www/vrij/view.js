e26bfcecc6fad1bc0da93729dbaa12fb51142a97
itsryan070 <itsryan070@gmail.com>
2019-02-11 13:35:13 +0200

    issue 78, 77 fix

--- www/vrij/view.js
+++ www/vrij/view.js
@@ -126,8 +126,9 @@ class VrijView
             totalproducts += relations[pr]['quantity'];
         }
 
-        html += "<div class='ui-popup-container pop in ui-popup-active' id='info-popup' style='max-width: 330px; top: 171px; left: 29px'>"
-                  + "<div class='ui-popup ui-body-inherit ui-overlay-shadow ui-corner-all' data-role='popup' id='infop' data-dismissible='false' style='max-width:400px; min-width: 300px'>"
+        html += "<div class='ui-popup-screen ui-overlay-inherit in' id='infop'></div> "
+        + "<div class='ui-popup-container pop in ui-popup-active' id='info-popup' style='top: 171px; left: 29px'>"
+                  + "<div class='ui-popup ui-body-inherit ui-overlay-shadow ui-corner-all' data-role='popup' id='infop' data-dismissible='false' style=''>"
                   + "<a onclick='c.closeInfoPopup()' style='position:relative; float: right;margin:0'  data-role='button'  class='ui-btn ui-shadow ui-corner-all ui-icon-delete ui-btn-icon-notext ui-btn-right ui-red' ></a>"
                   + "<h3 style='margin:0;margin-left:2vw; margin-top:1vh;'>Info</h3>"
                   + " <table id='info' data-role='table' class='ui-responsive table-stroke ui-table ui-table-reflow'>"
@@ -226,6 +227,7 @@ class VrijView
 
     closeInfoPopup() 
     {
+      $("#infop").remove();
       $("#info-popup").remove();
     }
     
